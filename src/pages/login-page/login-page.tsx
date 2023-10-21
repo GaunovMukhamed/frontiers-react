0950a618612635036874afc0053feb363e6e0cd9
SPACERIDER <spacerider@example.com>
2023-10-21 18:44:43 +0300

    add login page

--- src/pages/login-page/login-page.tsx
+++ src/pages/login-page/login-page.tsx
@@ -1,11 +1,26 @@
+import styles from "./login-page.module.scss";
 import { InputText } from 'primereact/inputtext';
+import { Password } from 'primereact/password';
 import { Button } from 'primereact/button';
+import { SyntheticEvent, useState } from "react";
         
 const LoginPage = () => {
+
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event: SyntheticEvent): void => {
+    event.preventDefault();
+    console.log(event)
+  }
+
   return (
-    <div>
-      <InputText />
-      <Button label="Submit"/>
+    <div className={styles.pageContainer}>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <InputText placeholder="Логин..." value={login} onChange={(e) => setLogin(e.target.value)} required />
+        <Password placeholder="Пароль..." value={password} onChange={(e) => setPassword(e.target.value)} feedback={false} toggleMask required />
+        <Button label="Войти" type="submit" />
+      </form>
     </div>
   );
 }
