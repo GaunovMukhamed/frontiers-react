9f2870ead7e5afa06705da7c54dd79985273d5d3
SPACERIDER <spacerider@example.com>
2023-10-21 14:05:50 +0300

    update routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter, redirect } from 'react-router-dom';
 import LoginPage from './pages/login-page/login-page';
 import styles from "./App.module.scss";
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      loader: () => redirect("/login"),
+    },
+    {
+      path: "/login",
+      element: <LoginPage />,
+    },
+    {
+      path: "*",
+      loader: () => redirect(`/login`),
+    }
+  ]);
+
   return (
     <div className={styles.mainContainer}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="login" element={<LoginPage />} />
-        </Routes>
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </div>
   );
 }
